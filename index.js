8f4659733eb48184180fad02ae46e030e26a6b52
OgembaJr <emmanuel.ogemba@student.moringaschool.com>
2022-11-28 22:04:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+// //Code your solutions in this file
+
+function writeCards(arrayOfNames, eventName) {
+    let card = []
+    for (let i = 0; i < arrayOfNames.length; i++) {
+        card.push(`Thank you, ${arrayOfNames[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return card
+}
+
+function countDown(positiveInteger) {
+    while (positiveInteger > 0 ) {
+        console.log(positiveInteger)
+        positiveInteger -= 1
+    }
+    console.log(positiveInteger)
+}
